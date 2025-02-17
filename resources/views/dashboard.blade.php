0d72d3e0f83a2cd6006c9f1e196f0169136768dd
Brian Nguyen <brianguyen177@gmail.com>
2025-02-17 16:41:18 -0500

    Cooking up squares in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,16 +5,24 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <!-- Make smaller boxes for different components to fit into -->
-        <div class="max-w-2xl h-40 sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        {{ __("Debt/Income ratio") }}
-                    </div>
+    <!-- Make smaller boxes for different components to fit into.  Aligns them horizontally-->
+    <div class="flow-root mt-12 mx-4">
+        <!-- For the debt/income ratio -->
+        <div class="float-left w-40 rounded-lg bg-white dark:bg-gray-800 shadow-sm">
+            <div class="p-6 text-gray-900 dark:text-gray-100">
+                {{ __("Debt/Income ratio graph here") }}
             </div>
         </div>
-        <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
+        <!-- For the rewards section -->
+        <div class="float-right w-40 rounded-lg bg-white dark:bg-gray-800 shadow-sm">
+            <div class="p-6 text-gray-900 dark:text-gray-100">
+                {{ __("Rewards go here") }}
+            </div>
+        </div>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-8xl mx-auto sm:px-6 lg:px-8 py-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <!-- Required for modal suport {{ __("You're logged in!") }}-->
@@ -24,4 +32,6 @@
             </div>
         </div>
     </div>
+
+
 </x-app-layout>
