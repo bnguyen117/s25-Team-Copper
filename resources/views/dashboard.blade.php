b35b04ba6ec899d35e015c708a06286a86c425c1
Brian Nguyen <brianguyen177@gmail.com>
2025-02-17 10:11:12 -0500

    "Playing around with making div boxes"

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,15 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <!-- Make smaller boxes for different components to fit into -->
+        <div class="max-w-2xl h-40 sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        {{ __("Debt/Income ratio") }}
+                    </div>
+            </div>
+        </div>
+        <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <!-- Required for modal suport {{ __("You're logged in!") }}-->
