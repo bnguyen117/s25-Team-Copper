2f644dae36aaf38b7e36f5e64de9770909e11b1a
Bryce <bjohn085@odu.edu>
2025-02-17 21:07:26 -0500

    Added reward and Goal Buttons for Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-center">
+            {{ Auth::user()->name }}'s Dashboard
         </h2>
     </x-slot>
 
@@ -10,22 +10,31 @@
         <!-- For the debt/income ratio -->
         <div class="float-left w-40 rounded-lg bg-white dark:bg-gray-800 shadow-sm">
             <div class="p-6 text-gray-900 dark:text-gray-100">
-                {{ __("Debt/Income ratio graph here") }}
+                {{ ("Debt/Income ratio graph here") }}
             </div>
         </div>
-        <!-- For the rewards section -->
+        <!-- Rewards and Leaderboard -->
         <div class="float-right w-40 rounded-lg bg-white dark:bg-gray-800 shadow-sm">
-            <div class="p-6 text-gray-900 dark:text-gray-100">
-                {{ __("Rewards go here") }}
+                 <div class="p-6 text-gray-900 dark:text-gray-100">
+                     <h3 class="text-black-600"> You've new rewards <span class="text-black-500"></span></h3>
+                       <a href="{{ url('http://s25-team-copper.test/rewards') }}" class="mt-3 px-4 py-2 bg-blue-600 text-white rounded-lg block text-center">Rewards</a>
             </div>
         </div>
     </div>
+
+        <!-- Your Goal Progress -->
+        <div class="bg-white dark:bg-gray-800 shadow-md rounded-lg p-6">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-100">Your Goal Progress</h3>
+                <a href="{{ url('http://s25-team-copper.test/finance') }}" class="mt-3 px-4 py-2 bg-blue-600 text-white rounded-lg block text-center">Edit Goals</a>
+            </div>
+        </div>
+
 
     <div class="py-12">
         <div class="max-w-8xl mx-auto sm:px-6 lg:px-8 py-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <!-- Required for modal suport {{ __("You're logged in!") }}-->
+                    <!-- Required for modal suport {{ ("You're logged in!") }}-->
                      @livewire('budget-form')
 
                 </div>
