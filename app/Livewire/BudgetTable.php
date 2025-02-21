6cc4ab14cecc67ca30e7ff7be18e9c6d443ce064
Josh Martin <jmart137@odu.edu>
2025-02-21 00:43:07 -0500

    Budget CRUD working

--- app/Livewire/BudgetTable.php
+++ app/Livewire/BudgetTable.php
@@ -3,13 +3,29 @@
 namespace App\Livewire;
 
 use App\Models\Budget;
+use Livewire\Component;
+
+// Form
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
+
+// Table
 use Filament\Tables;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
-use Livewire\Component;
+use Filament\Tables\Actions\BulkAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Columns\Layout\Split;
+use Filament\Tables\Columns\Layout\Panel;
+
+// Illuminate
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
@@ -24,6 +40,7 @@ class BudgetTable extends Component implements HasForms, HasTable
         return $table
             ->query(Budget::where('user_id', Auth::id()))
             ->heading('Budgets')
+            ->paginated(false)
             ->columns([
                 Tables\Columns\TextColumn::make('income')
                     ->numeric()
@@ -41,14 +58,110 @@ class BudgetTable extends Component implements HasForms, HasTable
             ->filters([
                 //
             ])
+            ->headerActions([
+                CreateAction::make()
+                    ->button()
+                    ->slideOver()
+                    ->form($this->getFormFields())
+                    ->mutateFormDataUsing(function (array $data): array {
+                        $data['user_id'] = Auth::id();
+                        return $data;
+                    }),
+            ])
             ->actions([
-                //
+                EditAction::make()
+                    ->button()
+                    ->slideOver()
+                    ->form($this->getFormFields()),
+                DeleteAction::make()
+                    ->button(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    //
+                BulkActionGroup::make([
+                    BulkAction::make('delete')
+                        ->requiresConfirmation()
+                        ->action(fn (Collection $records) => $records->each->delete())
                 ]),
             ]);
+    }
+
+    /**
+     * Defines and returns the table's columns as an array.
+     * Note: (Thank you Darrick!)
+     */
+    private function getBudgetTableColumns(): array 
+    {
+        return
+        [
+            Panel::make([
+                Split::make([
+                    TextColumn::make('income')
+                        ->numeric()
+                        ->description('Monthly Income', position: 'above')
+                        ->money('usd')
+                        ->sortable()
+                        ->weight(FontWeight::Medium),
+
+                    TextColumn::make('expenses')
+                        ->numeric()
+                        ->description('Monthly Expenses', position: 'above')
+                        ->money('usd')
+                        ->sortable()
+                        ->weight(FontWeight::Medium),
+
+                    TextColumn::make('savings')
+                        ->numeric()
+                        ->description('Monthly Savings', position: 'above')
+                        ->money('usd')
+                        ->sortable()
+                        ->weight(FontWeight::Medium),
+
+                    // Note: Will not be user-entered after testing
+                    TextColumn::make('remaining_balance')
+                        ->numeric()
+                        ->description('Remaining Balance', position: 'above')
+                        ->money('usd')
+                        ->sortable()
+                        ->weight(FontWeight::Medium),
+
+                ])->from('lg'),
+            ])->collapsible(),
+        ];
+    }
+
+    /**
+     * Defines and returns the form fields for creating and editing debts.
+     * Note: (Thank you Darrick!)
+     */
+    private function getFormFields(): array
+    {
+        return
+        [
+            TextInput::make('income')
+                ->required()
+                ->numeric()
+                ->minValue(0)
+                ->prefix('$'),
+
+            TextInput::make('expenses')
+                ->required()
+                ->numeric()
+                ->minValue(0)
+                ->prefix('$'),
+
+            TextInput::make('savings')
+                ->required()
+                ->numeric()
+                ->minValue(0)
+                ->prefix('$'),
+
+            TextInput::make('remaining_balance')
+                ->required()
+                ->numeric()
+                ->minValue(0)
+                ->prefix('$'),
+            
+        ];
     }
 
     public function render(): View
