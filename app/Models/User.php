ce9358700cda7a435ac30a580669ae23b0f0218b
Darrick Allen <dalle039@odu.edu>
2025-03-13 12:04:27 -0400

    Add first_login to the User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,7 @@ class User extends Authenticatable
         'budget',
         'avatar',
         'password',
+        'first_login'
     ];
 
     /**
@@ -47,6 +48,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'first_login' => 'boolean',
         ];
     }
 
