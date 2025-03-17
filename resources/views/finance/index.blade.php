cd6ae6e3629be77fcec67704d0b8a1c822b9d507
Josh Martin <jmart137@odu.edu>
2025-03-17 05:21:39 -0400

    Revert "Budget table rewritten"
    This reverts commit d81f326be0eb74896de10a43140572bbc96d5dbe.

--- resources/views/finance/index.blade.php
+++ resources/views/finance/index.blade.php
@@ -8,8 +8,6 @@
         </h2>
     </x-slot>
 
-    <!-- Budget Chart and Button to Budget Page-->
-    
     <!-- Main content container holding the Filament form -->
     <div class="mt-16 mb-16 m-auto max-w-[21rem] sm:max-w-sm md:max-w-2xl lg:max-w-7xl lg:px-8">
         @livewire('user-debt-table')
