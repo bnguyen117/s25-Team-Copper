4dfa2c59addc608435236aa1ae44ff16bb047d80
Darrick Allen <dalle039@odu.edu>
2025-04-09 18:03:14 -0400

    Implement budget bot

---
+++ database/migrations/2025_04_05_232054_update_budgets_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('budgets');
+        Schema::create('budgets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->decimal('income', 10, 2);
+            $table->decimal('needs_percentage', 5, 2)->nullable();
+            $table->decimal('wants_percentage', 5, 2)->nullable();
+            $table->decimal('savings_percentage', 5, 2)->nullable();
+            $table->decimal('budgeted_needs', 10, 2)->nullable();
+            $table->decimal('budgeted_wants', 10, 2)->nullable();
+            $table->decimal('budgeted_savings', 10, 2)->nullable();
+            $table->decimal('needs_progress', 10, 2)->nullable();
+            $table->decimal('wants_progress', 10, 2)->nullable();
+            $table->decimal('savings_progress', 10, 2)->nullable();
+            $table->decimal('remaining_balance', 10, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('budgets');
+    }
+};
