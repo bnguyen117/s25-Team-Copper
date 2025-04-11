8113d636b50167daf8dcb6dc0b9e7e936f57180f
erika-a13 <eandr003@odu.edu>
2025-04-10 23:52:14 -0400

    remove all GameLayer-related files/integration

--- config/services.php
+++ config/services.php
@@ -34,8 +34,5 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
-
-    'gamelayer' => [
-        'api_key' => env('GAMELAYER_API_KEY'),
-    ],
+    
 ];
